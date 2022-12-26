2c08573bea283b4175298935750361313aa8830d
Daniela1421 <daniela.ducuara1817@gmail.com>
2022-12-26 09:06:51 -0500

    feat: pokemon search view structure and styles

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
 import 'react-native-gesture-handler';
 import React from 'react'
-import { Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
-import { Navigator } from './src/navigator/Navigator';
+import { Tabs } from './src/navigator/Tabs';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Navigator/>
+      <Tabs/>
     </NavigationContainer>
   )
 }
